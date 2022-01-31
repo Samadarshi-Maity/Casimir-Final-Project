acb43b1399ec242149b3279c6d0e85a10a9bbc6b
JoseSotoGarcia <josesotogarci@gmail.com>
2022-01-31 16:04:02 +0100

    New changes

--- Dynamic_equations.py
+++ Dynamic_equations.py
@@ -25,9 +25,22 @@ t = np.linspace(0,tmax,0.01)
 
 # DEDUCED VARIABLES
 
-def rates(T,tau,R,Pi):
+def rates(Variables,t):
+    """
+    This function represent the set of differential equations to that wil
+    be solved.
+    Input:
+        Variables: T,tau,R, Pi
+    Output:
+        
+    """
+    T = Variables[0]
+    tau = Variables[1]
+    R = Variables[2]
+    Pi = Variables[3]
+    
     dT_dt = gamma - mu - mu*T
     dtau_dt = mu - gamma + mu*xi*Ro*kPitoR/(kPitoR + Pi) - mu*tau
     dR_dt = mu*Ro*(kPitoR/kPitoR + Pi) - mu*R
     dPi_dt = Pio*Rstarved + O-(delta + M)*Pi
-    return (dT_dt,dtau_dt,dR_dt,dPi_dt)
+    return [dT_dt,dtau_dt,dR_dt,dPi_dt]
