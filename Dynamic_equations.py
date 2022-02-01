9f4a287f29afdc320c62c9d64c6eef0edc308ab2
Milan Lacassin <milan.lacassin@gmail.com>
2022-02-01 17:05:27 +0100

    Resolving conflict of Sam and me trying to make the same modification at the same time

--- Dynamic_equations.py
+++ Dynamic_equations.py
@@ -39,10 +39,6 @@ def rates(Variables,t,I=0,M=0,N=0,O=0):
     Rstarved = kTOnMu/(kTOnMu + T)
     dT_dt = gamma - mu - mu*T
     dtau_dt = mu - gamma + mu*xi*Ro*kPitoR/(kPitoR + Pi) - mu*tau
-<<<<<<< HEAD
-    dR_dt = mu*Ro*kPitoR/(kPitoR + Pi) - mu*R
-=======
     dR_dt = mu*Ro*(kPitoR/(kPitoR + Pi)) - mu*R
->>>>>>> e98189d8528f79b83820c171513378d66a52b0cc
     dPi_dt = Pi0*Rstarved + O-(delta + M)*Pi
     return [dT_dt,dtau_dt,dR_dt,dPi_dt]
